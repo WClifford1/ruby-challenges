618dc7f0d3c54137dea31927895466f4e7db1a0b
WClifford1 <williamdavidclifford@gmail.com>
2019-02-28 14:23:28 +1000

    Completed time_and_bar

--- time_and_bar.rb
+++ time_and_bar.rb
@@ -1,26 +1,26 @@
 system('clear')
+=begin
+#----PART 1----
 
+#I created a class in order to practice utilizing classes and methods
 class Part1
-    attr_accessor :commuteTime
+    attr_reader :commuteTime
     def initialize
         @commuteTime = commuteTime
     end
 
     def output
         @commuteTime = 15
-        return "I rode my motorbike into work and parked on one of the sidestreets nearby, it took me no more than #{commuteTime} minutes to get to class."
+        return "I rode my motorbike into work and parked on one of the sidestreets nearby, it took me about #{commuteTime} minutes to get to class.\n"
     end
 end
 
 commute = Part1.new
 puts commute.output
-puts
-puts "I have commented out the second and third parts of the question in the class below. Please uncomment them to read, thank you"
-puts
+
+=end
 
 
-
-=begin
 #Define the program as a class
 class Part2
 
@@ -29,7 +29,6 @@ attr_accessor :waterNumber, :beerNumber, :cocktailNumber
 
 #Initialize the variables used in the program as instance varaibles
     def initialize
-        
         @waterNumber = waterNumber
         @beerNumber = beerNumber
         @cocktailNumber = cocktailNumber
@@ -40,39 +39,43 @@ attr_accessor :waterNumber, :beerNumber, :cocktailNumber
         @cocktailNumber = 3
         @waterNumber = 2
         @beerNumber = 6
-        puts "Which drink do you want?"
-        puts
+        puts "What type of drink would you like? We sell cocktails, beers, and water.\n\n"
         drink = gets.chomp
         drink.downcase!
-            
-            #Add if / else conditions for what the user types in.
-            #Asks the user what drink they want, how many, adds this to the total
+        
+        #Asks the user what drink they want, how many, adds this to the total
+        #Add if / else conditions for what the user types in.
+        
+
+            #If the 
             if
                 drink.include? "cocktail"
-                puts "How many cocktails do you want?"
-                puts
+                system('clear')
+                puts "How many cocktails do you want?\n\n"
                 @cocktailNumber += gets.to_i
+                system('clear')
             
             elsif
                 drink.include? "water"
-                puts "How many waters do you want?"
-                puts
+                system('clear')
+                puts "How many waters do you want?\n\n"
                 @waterNumber += gets.to_i
+                system('clear')
             
             elsif
                 drink.include? "beer"
-                puts "How many beers do you want?"
-                puts
+                system('clear')
+                puts "How many beers do you want?\n\n"
                 @beerNumber += gets.to_i
+                system('clear')
             
             else 
                 drink != "water" or "beer" or "cocktail"
-                puts "We don't sell that, we only sell cocktails, beers, or waters"
-                puts
+                system('clear')
+                puts "We don't sell that, we only sell cocktails, beers, or waters.\n\n"
             end
     
-    puts
-    return "You have #{@cocktailNumber} cocktails, #{@waterNumber} waters and #{@beerNumber} beers to make"
+    return "You have #{@cocktailNumber} cocktails, #{@waterNumber} waters and #{@beerNumber} beers to make."
     
     end
 
@@ -83,7 +86,7 @@ attr_accessor :waterNumber, :beerNumber, :cocktailNumber
         #The number of waters has to be changed to a float and made to have 2 decimal places
         @waterNumber = @waterNumber.to_f
         puts
-        return "You have made $#{@cocktailNumber * 8} profit on cocktails, $#{'%.2f' % (@waterNumber * 5.85)} profit on waters, and $#{@beerNumber * 9} profit on beers"
+        return "So far you have made $#{@cocktailNumber * 8} profit on cocktails, $#{'%.2f' % (@waterNumber * 5.85)} profit on waters, and $#{@beerNumber * 9} profit on beers.\n\n"
     end
 
 end
@@ -92,4 +95,3 @@ end
 part2 = Part2.new
 puts part2.customerOrder
 puts part2.profit
-=end
