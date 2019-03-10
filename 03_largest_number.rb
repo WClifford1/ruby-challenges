5ecf96117ea0af53f01c3ecea94f041c8217c5a3
WClifford1 <williamdavidclifford@gmail.com>
2019-03-11 09:06:16 +1000

    Completed 03_largest_number

--- 03_largest_number.rb
+++ 03_largest_number.rb
@@ -1,6 +1,6 @@
 # Write a method that will take two numbers,
 # and return whichever is the largest.
-#
+##
 # Example:
 # largest_number(100,50) should return 100
 # largest_number(10,20) should return 20
