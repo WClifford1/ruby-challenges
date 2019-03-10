234c9d8a50baa1c88acee3775df0c7fe111dcbe9
WClifford1 <williamdavidclifford@gmail.com>
2019-03-10 16:15:13 +1000

    Completed 06_definition

--- 06_definition.rb
+++ 06_definition.rb
@@ -13,30 +13,30 @@
 #
 # Check your solution by running the tests:
 # ruby tests/06_definition_test.rb
-system('clear')
 class Definition
-
-    attr_accessor :word, :definition, :dictionary, :dictionary2
-
-def initialize
-    @dictionary = dictionary
-    @dictionary2 = dictionary2
-    @word = word
-    @definition = @definition
-end
-
-def add_word(word, definition)
-
-    dictionary = Hash.new
-    dictionary2 = Hash.new
-    dictionary["#{word}"] = "#{definition}"
-    dictionary2.merge(dictionary)
-    return puts dictionary2
-
-end    
-
-
-end
-
-definition = Definition.new
-puts definition.add_word("Ruby", "a red, precious stone")
+    attr_accessor :dictionary
+    
+        def initialize
+            @dictionary = {}
+    
+        end
+    
+        def add_word(word, definition)
+    
+            return dictionary[word] = definition
+    
+        end
+    
+        def total_words
+    
+            return dictionary.length
+    
+        end
+    
+        def lookup(word)
+    
+            return dictionary[word]
+    
+        end
+    
+    end
